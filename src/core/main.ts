fbf05d28348cae01dc1be2579ee7fcf188b274b1
MiguelAP30 <miguelonperez300@gmail.com>
2025-03-03 00:36:52 -0500

    cors del frontend

--- src/core/main.ts
+++ src/core/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitar CORS para permitir peticiones desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:4200', // Permitir solo tu frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization' // Encabezados permitidos
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
